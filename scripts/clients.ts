da0b441c45d935a16ec74659b4f8a9658601cb6e
Joaquin Failo <73504599+Joafai@users.noreply.github.com>
2026-04-28 12:56:11 -0300

    added x layer

--- scripts/clients.ts
+++ scripts/clients.ts
@@ -1,17 +1,15 @@
 import {HttpTransportConfig} from 'viem';
-import {ChainId, getClient as rpcEnvGetClient} from '@bgd-labs/toolbox';
+import {getClient as rpcEnvGetClient} from '@bgd-labs/toolbox';
 
 const commonConfig: HttpTransportConfig = {timeout: 30_000, batch: true};
 const batchConfig = {batch: {multicall: true}};
-
-const ALCHEMY_UNSUPPORTED_CHAINS = new Set<number>([ChainId.xLayer]);
 
 export function getClient(chainId: number) {
   return rpcEnvGetClient(chainId, {
     clientConfig: batchConfig,
     httpConfig: commonConfig,
     providerConfig: {
-      alchemyKey: ALCHEMY_UNSUPPORTED_CHAINS.has(chainId) ? undefined : process.env.ALCHEMY_API_KEY,
+      alchemyKey: process.env.ALCHEMY_API_KEY,
       quicknodeEndpointName: process.env.QUICKNODE_ENDPOINT_NAME,
       quicknodeToken: process.env.QUICKNODE_TOKEN,
     },
